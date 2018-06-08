ae3dc522566b4569dae4cfc99e5e11bbb47a164a
LolloDeniz <denisilorenzo@gmail.com>
2018-06-08 19:46:42 +0200

    Initial Commit

--- src/JMines.java
+++ src/JMines.java
@@ -3,7 +3,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
-import java.lang.management.MemoryNotificationInfo;
 import javax.swing.*;
 import javax.swing.border.EtchedBorder;
 
